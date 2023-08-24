a33186a0b70989df63be3f1852eb0b3de0dee830
이다현 <68106298+dahyeon-da@users.noreply.github.com>
2023-08-24 17:33:50 +0900

    initial setting

--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// id가 root인 곳을 가리키는 거임. id 바꾸려면 여기랑 같이 바꿔라~
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+// 여기있는 APP이 컴포넌트이다.
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
