d7b66cb6871e1286f0705392df914db32653c9fd
이다현 <68106298+dahyeon-da@users.noreply.github.com>
2023-08-29 23:50:16 +0900

    event_create 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
-import TOC from "./components/TOC";
-import Content from "./components/Content";
-import Subject from "./components/Subject";
-import Control from "./components/Control";
+import TOC from "./components/TOC"
+import ReadContent from "./components/ReadContent"
+import CreateContent from "./components/CreateContent"
+import Subject from "./components/Subject"
+import Control from "./components/Control"
 import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
+    this.max_content_id = 3;
     this.state = {
-      mode: 'read',
+      mode: 'create',
       selected_content_id: 2,
       subject: { title: 'WEB', sub: 'world wide web!' },
       welcome: { title: 'welcome', desc: 'Hello React!!' },
@@ -21,11 +23,12 @@ class App extends Component {
     }
   }
   render() {
-    var _title, _desc = null;
+    var _title, _desc, _article = null;
 
     if (this.state.mode === 'welcome') {
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>;
     } else if (this.state.mode === 'read') {
       var i = 0;
       while (i < this.state.contents.length) {
@@ -36,6 +39,26 @@ class App extends Component {
         }
         i = i + 1;
       }
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>;
+    } else if (this.state.mode === 'create') {
+      _article = <CreateContent onSubmit={function(_title, _desc){
+        // add content to this.state.contents
+        this.max_content_id = this.max_content_id + 1;
+        // this.state.contents.push(
+        //   {id:this.max_content_id, title:_title, desc:_desc}
+        // );
+        // this.setState({
+        //   contents:this.state.contents
+        // });
+        var _contents = this.state.contents.concat(
+          {id:this.max_content_id, title:_title, desc:_desc}
+        )
+        this.setState({
+          contents:_contents
+        });
+        console.log(_title);
+        console.log(_desc);
+      }.bind(this)}></CreateContent>
     }
 
     return (
@@ -63,10 +86,7 @@ class App extends Component {
           });
         }.bind(this)}>
         </Control>
-        <Content
-          title={_title}
-          desc={_desc}
-        ></Content>
+        {_article}
       </div>
     );
   }
